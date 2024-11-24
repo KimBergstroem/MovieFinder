0804bc241cb94acbe8ae30675ea83da6a54809fb
KimBergstroem <kimmen_b@hotmail.com>
2024-11-24 18:21:09 +0100

    update: error lang from Swe to Eng

--- src/apis/fetchMovies.js
+++ src/apis/fetchMovies.js
@@ -3,12 +3,12 @@ export default async function fetchMovies(page = 1) {
     const url = `${import.meta.env.VITE_BASE_URL}${import.meta.env.VITE_API_KEY}&page=${page}`;
     const response = await fetch(url);
     if (!response.ok) {
-      throw new Error("Något gick fel med att hämta filmerna");
+      throw new Error("Something went wrong with fetching the movies");
     }
     const data = await response.json();
     return data;
   } catch (error) {
-    console.error("Fel vid hämtning av filmer:", error);
+    console.error("Error fetching movies:", error);
     throw error;
   }
 }
